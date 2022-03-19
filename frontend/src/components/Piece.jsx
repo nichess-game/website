edc0e00141a8576057959dfe929bed997e75eb58
Vasilije <73365189+vasilije2448@users.noreply.github.com>
2022-03-19 17:01:50 +0100

    better mobile view

--- frontend/src/components/Piece.jsx
+++ frontend/src/components/Piece.jsx
@@ -1,16 +1,19 @@
 import React from 'react'
 
-//export default function Piece({ piece: { type, player } }) {
-
 export default function Piece({ piece }) {
   const pieceImg = require (`../assets/${piece.type}_${piece.player}.png`)
+  var pieceHealthClass = 'piece-health'
+  if (/Mobi|Android/i.test(navigator.userAgent)) { // TODO: Write better solution for mobile.
+    pieceHealthClass = 'piece-health-mobile'
+  }
   return ( 
     <div>
     <div className="piece-container">
       {piece.type != 'empty' &&
-      <div className='piece-health'>{piece.healthPoints} | {piece.abilityPoints}</div>
+      <div className={pieceHealthClass}>{piece.healthPoints} | {piece.abilityPoints}</div>
       }
     </div>
+
     <div className="piece-container">
       <img src={ pieceImg } alt="" className="piece"/>
     </div>
--- frontend/src/views/PlayAgainstTheWorld.jsx
+++ frontend/src/views/PlayAgainstTheWorld.jsx
@@ -17,10 +17,15 @@ function PlayAgainstTheWorld() {
     return () => subscribe.unsubscribe()
   }, [])
 
+  var boardContainerClass = 'board-container'
+  if (/Mobi|Android/i.test(navigator.userAgent)) { // TODO: Write better solution for mobile.
+    boardContainerClass = 'board-container-mobile'
+  }
+
   return (
     <div>
       <div className="container">
-        <div className="board-container">
+        <div className={boardContainerClass}>
           <BoardForGlobalGame board={board} game={game} />
         </div>
       </div>
